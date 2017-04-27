3dbd68df8881e3156aa53e0858c2af9b8343cba0
Rodrigo Salles <roosalles@gmail.com>
2017-04-26 21:54:32 -0300

    Created plugin

---
+++ toptal-social-share.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Toptal Social Share
+ * Plugin URI: https://toptal.com/
+ * Description: This plugin will automatically display selected social network(s) sharing buttons in posts and/or on pages.
+ * Version: 1.0.0
+ * Author: Rodrigo Salles
+ * Author URI: http://rodrigosalles.com
+ * License: GPL2
+  */
