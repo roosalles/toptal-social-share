f36a60dfeec19342687127527c3683a1140a3f0b
Rodrigo Salles <rodrigo@rprojectwebstudio.com>
2022-12-26 11:30:48 -0300

    Fixed trailing spaces and formatting according to WPCS.

--- js/admin.js
+++ js/admin.js
@@ -5,24 +5,24 @@ jQuery( document ).ready( function( $ ) {
 
 	// Function to update our hidden field (ordered networks)
 	var updateOrder = function( event, ui ) {
-		
+
 		var $network = $( '#tss-sortable-networks' ).sortable( 'toArray', { attribute: 'data-network' } )
 		$( '#tss-ordered-networks' ).val( $network );
-	}	
+	}
 
 	// Make list of networks sortable, update field when order is changed.
 	$( '#tss-sortable-networks' ).sortable( {
-		create: updateOrder,		
+		create: updateOrder,
 		update: updateOrder
-	});
-	
+	} );
+
 	// Execute wpColorPicker for all color picker fields
 	$( '#tss-color-picker' ).wpColorPicker();
 	$( '#tss-bg-color-picker' ).wpColorPicker();
-	
+
 	// Show/Hide Color picker on user selection/click
 	$( '#tss-use-custom-color-field' ).on( 'click', function() {
 		$( '#tss-color-picker-wrapper' ).slideToggle();
-	});
-	
-});
+	} );
+
+} );
